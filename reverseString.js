2dd93f607eb600c8f220edf25a4c69e11a50be76
neillsom <neillsom@mail.com>
2018-06-18 09:28:00 -0700

    Iterative solution

--- reverseString.js
+++ reverseString.js
@@ -2,9 +2,21 @@ function reverseString(str) {
   if (str.length === 1) {
     return str;
   }
-  console.log(str); 
+
   return reverseString(str.slice(1)) + str[0]
 
 }
 
 console.log(reverseString('Hello'))
+
+// Iterative solution
+
+function iReverseString(str) {
+  let accumulator = '';
+  while (str !== '') {
+    accumulator = str[0] + accumulator;
+    str = str.slice(1);
+  }
+  return accumulator
+}
+console.log(iReverseString('Howdy'))
