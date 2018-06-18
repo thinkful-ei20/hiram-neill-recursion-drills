25f89cacb138cf527c1f9ba4ef2c3d1e758f25d8
Hiram Cruz <hiram0101@gmail.com>
2018-06-18 12:01:59 -0400

    added iterative method

--- array_double.js
+++ array_double.js
@@ -3,8 +3,12 @@ function doubleArray(arr) {
   return [arr[0] * 2, ...doubleArray(arr.slice(1))]
 }
 
+function doubleArrayIter(arr) {
+  return arr.map(val => val * 2)
+}
+
 // [1, 2, 3]
 // [2, 4, 6]
 
 console.log(doubleArray([1, 2, 3]))
-console.log(doubleArray([5, 10, 20]))
+console.log(doubleArrayIter([5, 10, 20]))
